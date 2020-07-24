7473f50aece33dcf4c654161286da8e5d0e71a24
Filipe Sousa dos Santos <filpess@hotmail.com>
2020-07-23 21:07:30 -0300

    CRUD Usuário

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,26 +1,60 @@
-import {getRepository} from "typeorm";
-import {NextFunction, Request, Response} from "express";
-import {User} from "../models/User";
+import { getRepository } from "typeorm";
+import { Request, Response } from "express";
+import { User } from "../models/User";
+import { PasswordCrypto } from "../utils/crypto";
+import { Area } from "../models/Area";
 
-export class UserController {
-
-    private userRepository = getRepository(User);
-
-    async all(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.find();
+export default {
+  async index(request: Request, response: Response) {
+    try {
+      const user = await getRepository(User).find();
+      return response.json(user)
+    } catch (error) {
+      return console.error('Error ', error)
     }
-
-    async one(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.findOne(request.params.id);
+  },
+  async show(request: Request, response: Response) {
+    try {
+      const user = await getRepository(User).findOne(request.params.id)
+      return response.json(user)
+    } catch (error) {
+      return console.error('Error ', error)
     }
+  },
 
-    async save(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.save(request.body);
+  async create(request: Request, response: Response) {
+    try {
+      let { password, repeatPassword } = request.body
+      if (password !== repeatPassword) {
+        return response.json({mensagem: 'Senhas não são iguais'})
+      }
+      request.body.password = new PasswordCrypto(password).criptografar()
+      const area = await getRepository(Area).findOne(request.body.areaId) 
+      const user = await getRepository(User).save({...request.body, area})
+      return response.json(user)
+    } catch (error) {
+      console.error('Error ', error)
+      return response.json({message: error.message})
     }
-
-    async remove(request: Request, response: Response, next: NextFunction) {
-        let userToRemove = await this.userRepository.findOne(request.params.id);
-        await this.userRepository.remove(userToRemove);
+  },
+  async update(request: Request, response: Response) {
+    try {
+      let { id, nome, email, areaId } = request.body
+      const area = await getRepository(Area).findOne(areaId)       
+      const user = await getRepository(User).save({ id, nome, email, area })
+      return response.json(user)
+    } catch (error) {
+      console.error('Error ', error)
+      return response.status(500).json({message: error.message})
     }
-
+  },
+  async delete(request: Request, response: Response) {
+    try {
+      const user = await getRepository(User).findOne(request.params.id)
+      const result = await getRepository(User).remove(user) 
+      return response.json(result)
+    } catch (error) {
+      return console.error('Error ', error)
+    }
+  }
 }
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,19 +1,25 @@
-import {Entity, PrimaryGeneratedColumn, Column, ManyToOne} from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
 import { Area } from "./Area";
 
 @Entity()
 export class User {
 
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    name: string;
+  @PrimaryGeneratedColumn('uuid')
+  uuid: string;
 
-    @Column()
-    email: string;
+  @Column({nullable: false})
+  name: string;
 
-    @ManyToOne(type => Area, area => area.users)
-    area: Area
+  @Column({ unique: true, nullable: false })
+  email: string;
+  
+  @Column({nullable: false})
+  password: string;
+
+  @ManyToOne(type => Area, area => area.users)
+  area: Area
 
 }
